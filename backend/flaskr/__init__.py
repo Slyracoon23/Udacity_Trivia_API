ed79189be4f720e4b0b636d17d6cf8e50fb5f913
Earl Potters <earl.potters@gmail.com>
2020-06-17 10:52:20 -0600

    Finished Trivia_api

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -114,56 +114,173 @@ def create_app(test_config=None):
         })
 
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
 
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
+  @app.route('/questions', methods=['POST'])
+  def post_question():
+    ''' Handles POST requests for creating new questions and searching questions. '''
+    body = request.get_json()
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
+    # if search term is present
+    if body.get('searchTerm', None):
+      return search_questions()
+    else:
+      return add_question()
 
   '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+   @TODO: 
+   Create an endpoint to POST a new question, 
+   which will require the question and answer text, 
+   category, and difficulty score.
+
+   TEST: When you submit a question on the "Add" tab, 
+   the form will clear and the question will appear at the end of the last page
+   of the questions list in the "List" tab.  
+   '''
+
+
+  def add_question():
+    body = request.get_json()
+
+    query_list = ['question', 'answer', 'category', 'difficulty']
+
+    list_request = [body.get(query, None) for query in query_list]
+    print(list_request)
+    if all(list_request) is False:
+      abort(422)
+
+    try:
+      question = Question(*list_request) # unpack values
+
+      question.insert()
+
+      return jsonify({
+        'succes': True,
+        'created': question.id
+      })
+    except:
+      abort(422)
+
+
+  # @TODO:
+  # Create a POST endpoint to get questions based on a search term.
+  # It should return any questions for whom the search term
+  # is a substring of the question.
+  #
+  # TEST: Search by any phrase. The questions list will update to include
+  # only question that include that string within their question.
+  # Try using the word "title" to start.
+
+  def search_questions():
+    body = request.get_json()
+    query_searchTerm = body.get('searchTerm', None)
+
+    if query_searchTerm:
+      search_results = Question.query.filter(
+                  Question.question.ilike(f'%{query_searchTerm}%')).all()
+
+      return jsonify({
+        'success': True,
+        'questions': [question.format() for question in search_results],
+        'total_question': len(search_results),
+        'current_category': None
+      })
+    else:
+      abort(404)
+
+
+  # @TODO:
+  # Create a GET endpoint to get questions based on category.
+  #
+  # TEST: In the "List" tab / main screen, clicking on one of the
+  # categories in the left column will cause only questions of that
+  # category to be shown.
+
+  @app.route('/categories/<int:category_id>/questions')
+  def retrieve_questions_by_category(category_id):
+
+    try:
+      questions = Question.query.filter(Question.category == str(category_id)).all()
+
+      return jsonify({
+        'success': True,
+        'questions': [question.format() for question in questions],
+        'total_questions': len(questions),
+        'current_category' : category_id
+      })
+    except:
+      abort(404)
+
+
+
+
+
+  # @TODO:
+  # Create a POST endpoint to get questions to play the quiz.
+  # This endpoint should take category and previous question parameters
+  # and return a random questions within the given category,
+  # if provided, and that is not one of the previous questions.
+  #
+  # TEST: In the "Play" tab, after a user selects "All" or a category,
+  # one question at a time is displayed, the user is allowed to answer
+  # and shown whether they were correct or not.
   
-  return app
+  @app.route('/quizzes', methods=['POST'])
+  def get_random_quiz_question():
+     try:
+          body = request.get_json()
 
-    
+          previous_qustions = body.get('previous_questions', None)
+          category = body.get('quiz_category', None)
+          print(previous_qustions, category)
+          if (previous_qustions is None) or (category is None):
+              abort(422)
+
+          if category['id'] == 0: # All category
+              available_questions = Question.query.filter(Question.id.notin_(previous_qustions)).all()
+
+          else:
+              available_questions = Question.query.filter( Question.category == category['id'],
+                Question.id.notin_(previous_qustions)).all()
+
+          new_question = random.choice(available_questions) if available_questions else None
+
+          print(new_question)
+          return jsonify({
+              'success': True,
+              'question': new_question.format() if new_question else None
+            })
+     except:
+         abort(422)
+
+  # @TODO:
+  # Create error handlers for all expected errors
+  # including 404 and 422.
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False,
+          "error": 404,
+          "message": "resource not found"
+      }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          "success": False,
+          "error": 422,
+          "message": "unprocessable"
+      }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False,
+          "error": 400,
+          "message": "bad request"
+      }), 400
+
+
+
+  return app
